cce8d7683a3c35424f920fbc691e70971c4b0a60
SiddheshKukade <littelord124@gmail.com>
2021-07-07 17:23:18 +0000

    NN 2

---
+++ Neural Networks /mat.py
@@ -0,0 +1,10 @@
+# importing the required module 
+import matplotlib.pyplot as plt 
+    
+# x axis values 
+x = [1,2,3] 
+# corresponding y axis values 
+y = [2,4,1] 
+    
+# plotting the points  
+plt.plot(x, y) 
